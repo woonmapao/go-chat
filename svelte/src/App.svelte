ab5118976f2c83c33f6ab0ec779666a875b96881
Pao Patarapong <35144104+woonmapao@users.noreply.github.com>
2022-07-23 10:47:14 +0700

    Add files via upload

---
+++ svelte/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+    let username = "username";
+</script>
+
+<div class="container">
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+        <div
+            href="/"
+            class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom"
+        >
+            <input class="fs-5 fw-semibold" type="text" bind:value={username} />
+        </div>
+        <div class="list-group list-group-flush border-bottom scrollarea">
+            <a
+                href="#"
+                class="list-group-item list-group-item-action py-3 lh-sm"
+            >
+                <div
+                    class="d-flex w-100 align-items-center justify-content-between"
+                >
+                    <strong class="mb-1">List group item heading</strong>
+                    <small class="text-muted">Tues</small>
+                </div>
+                <div class="col-10 mb-1 small">
+                    Some placeholder content in a paragraph below the heading
+                    and date.
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
