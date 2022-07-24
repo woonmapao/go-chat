2ff91f83974c53475952f337df91219e0a16f6fc
Pao Patarapong <35144104+woonmapao@users.noreply.github.com>
2022-07-24 09:51:30 +0700

    Add files via upload

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,31 +1,70 @@
 <script>
+    import { onMount } from "svelte";
+    import Pusher from "pusher-js";
+
     let username = "username";
+    let message = "";
+    let messages = [];
+
+    onMount(() => {
+        Pusher.logToConsole = true;
+
+        const pusher = new Pusher("659c70e291e6e4b7fb80", {
+            cluster: "ap1",
+        });
+
+        const channel = pusher.subscribe("chat");
+        channel.bind("message", (data) => {
+            messages = [...messages, data];
+        });
+    });
+
+    async function submit() {
+        await self.fetch("http://localhost:8000/api/messages", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                username,
+                message,
+            }),
+        });
+        message = "";
+    }
 </script>
 
 <div class="container">
     <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
         <div
-            href="/"
             class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom"
         >
             <input class="fs-5 fw-semibold" type="text" bind:value={username} />
         </div>
         <div class="list-group list-group-flush border-bottom scrollarea">
-            <a
-                href="#"
-                class="list-group-item list-group-item-action py-3 lh-sm"
-            >
-                <div
-                    class="d-flex w-100 align-items-center justify-content-between"
-                >
-                    <strong class="mb-1">List group item heading</strong>
-                    <small class="text-muted">Tues</small>
+            {#each messages as msg}
+                <div class="list-group-item list-group-item-action py-3 lh-sm">
+                    <div
+                        class="d-flex w-100 align-items-center justify-content-between"
+                    >
+                        <strong class="mb-1">{msg.username}</strong>
+                    </div>
+                    <div class="col-10 mb-1 small">{msg.message}</div>
                 </div>
-                <div class="col-10 mb-1 small">
-                    Some placeholder content in a paragraph below the heading
-                    and date.
-                </div>
-            </a>
+            {/each}
         </div>
     </div>
+
+    <form on:submit|preventDefault={submit}>
+        <input
+            class="form-control"
+            placeholder="Write a message"
+            bind:value={message}
+        />
+        <input type="submit" />
+    </form>
 </div>
+
+<style>
+    .scrollarea {
+        min-height: 500px;
+    }
+</style>
